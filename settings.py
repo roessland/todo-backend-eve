be5c725d03dc1f3af82f826768742b9e2fe53280
Andreas Røssland <andreas.rossland@silabs.com>
2014-07-01 11:06:36 +0200

    Initial commit

---
+++ settings.py
@@ -0,0 +1,29 @@
+MONGO_HOST = 'localhost'
+MONGO_DBNAME = 'todo'
+
+RESOURCE_METHODS = ['GET', 'DELETE', 'POST']
+ITEM_METHODS = ['GET', 'PATCH', 'DELETE']
+
+todos = {
+    'schema': {
+        'text': {
+            'type': 'string',
+            'required': True,
+        },
+        'done': {
+            'type': 'boolean',
+            'required': True,
+        },
+    },
+}
+
+DOMAIN = {
+    'todos': todos,
+}
+
+# Disable etags. Will make accidental overwriting possible, but makes the
+# Angular JS code a lot easier.
+IF_MATCH = False
+
+# Makes requests return submitted/changed object
+BANDWIDTH_SAVER = False
